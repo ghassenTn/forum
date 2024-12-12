051b1307e207c3f0a2a462bc3e5503b5cf698c50
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2024-12-12 09:44:47 +0000

    final+1

--- main.py
+++ main.py
@@ -35,6 +35,24 @@ def bubels_plot():
 
     return fig
 
+def line_plot():
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3, 4], [10, 20, 15, 25], color='#1f77b4')
+    ax.set_title('Sample Line Chart')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_color('#dddddd')
+    ax.spines['bottom'].set_color('#dddddd')
+    ax.tick_params(axis='x', colors='#555555')
+    ax.tick_params(axis='y', colors='#555555')
+    ax.yaxis.grid(True, color='#eeeeee')
+    ax.xaxis.grid(False)
+    fig.tight_layout()
+
+    return fig
+
 st.markdown("## Debian Community .")
 st.caption("By @DebianCommunity :material/forum:")
 ### news about new release
@@ -50,6 +68,12 @@ with cnt_bubels:
     st.markdown("**Bubels chart new release**")
     st.caption("By @DebianCommunity :material/forum:")
     st.pyplot(bubels_plot())
+
+cnt_line = st.container(border=True)
+with cnt_line:
+    st.markdown("**Line chart new release**")
+    st.caption("By @DebianCommunity :material/forum:")
+    st.pyplot(line_plot())
 
         
         
