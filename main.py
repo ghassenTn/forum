d31adedecdc015f8b3d9a26f74295e6ad12ea7b6
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2024-12-12 11:21:34 +0000

    update sidebar

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ with sb:
 #     sac.rate(label='', value=3.5, align='center',half=True)
 #     sac.alert(label='ghassenTn ', description=' Welcome  ', banner=True, icon=False, closable=True,size='md',variant='quote')
     sac.menu([
-    sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), sac.Tag('Tag2', 'red')]),
+    sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), ]),
     sac.MenuItem('products', icon='box-fill', children=[
         sac.MenuItem('apple', icon='apple'),
         sac.MenuItem('other', icon='git', description='other items', children=[
@@ -157,13 +157,7 @@ with sb:
             sac.MenuItem('wechat', icon='wechat'),
         ]),
     ]),
-    sac.MenuItem('disabled', disabled=True),
-    sac.MenuItem(type='divider'),
-    sac.MenuItem('link', type='group', children=[
-        sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
-        sac.MenuItem('bootstrap-icon', icon='bootstrap-fill', href='https://icons.getbootstrap.com/'),
-    ]),
-], open_all=True)
+], open_all=False,indent=30)
     
     
     
