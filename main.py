65085aa1a3c2b1a6bb3bd353dced776ecced5820
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2024-12-12 11:15:07 +0000

    add cascader

--- main.py
+++ main.py
@@ -140,8 +140,6 @@ def line_plot():
 with sb:
     st.caption("**ghassenTn** :material/workspace_premium:")
     sac.divider(label='Social Media', icon='wechat', align='center', color='gray')
-
-
     sac.buttons([
     sac.ButtonsItem(icon='github',label='Github'),
     sac.ButtonsItem(label='wechat', icon='wechat'),
@@ -149,6 +147,25 @@ with sb:
 ], label='', align='center')
     sac.rate(label='', value=3.5, align='center',half=True)
     sac.alert(label='ghassenTn ', description=' Welcome  ', banner=True, icon=False, closable=True,size='md',variant='quote')
+
+    sac.cascader(items=[
+    sac.CasItem('home', icon='house'),
+    sac.CasItem('app', icon='app', children=[
+        sac.CasItem('store', icon='bag-check'),
+        sac.CasItem('brand', icon='award', children=[
+            sac.CasItem('github', icon='github'),
+            sac.CasItem('google', icon='google'),
+            sac.CasItem('apple', icon='apple', children=[
+                sac.CasItem('admin', icon='person-circle'),
+                sac.CasItem('guest', icon='person'),
+                sac.CasItem('twitter' * 5, icon='twitter'),
+            ]),
+        ]),
+    ]),
+    sac.CasItem('disabled', icon='send', disabled=True),
+    sac.CasItem('other1'),
+    sac.CasItem('other2')
+], label='label', index=0, multiple=True, search=True, clear=True)
 
 
     
