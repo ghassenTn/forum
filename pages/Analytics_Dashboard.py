1801290a50f111014acadfc2c3f4937798998779
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2024-12-12 10:01:32 +0000

    make logo for all pages

--- pages/Analytics_Dashboard.py
+++ pages/Analytics_Dashboard.py
@@ -1,5 +1,5 @@
 import streamlit as st
 cols = st.columns(2)
-
+st.logo("https://img.icons8.com/?size=100&id=uKPJ5HL65nlH&format=png&color=000000",size='large')
 with cols[0]:
     st.metric("Indicator",19,delta='%',delta_color='normal')
