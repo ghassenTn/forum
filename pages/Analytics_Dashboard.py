ded7604da9505151aa0ae90de983fff270e92bcf
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2024-12-12 10:04:06 +0000

    update analytics

--- pages/Analytics_Dashboard.py
+++ pages/Analytics_Dashboard.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import numpy as np
 cols = st.columns(2)
 st.logo("https://img.icons8.com/?size=100&id=uKPJ5HL65nlH&format=png&color=000000",size='large')
-with cols[0]:
-    st.metric("Indicator",19,delta='%',delta_color='normal')
+
+
+col1, col2 = st.columns([3, 1])
+data = np.random.randn(10, 1)
+
+col1.subheader("A wide column with a chart")
+col1.line_chart(data)
+
+col2.subheader("A narrow column with the data")
+col2.write(data)
