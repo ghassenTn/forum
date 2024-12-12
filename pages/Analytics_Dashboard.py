acc47ab9b7ab73442d60981c62ae0f332d4276fe
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2024-12-12 10:06:27 +0000

    update analytics

--- pages/Analytics_Dashboard.py
+++ pages/Analytics_Dashboard.py
@@ -4,11 +4,11 @@ cols = st.columns(2)
 st.logo("https://img.icons8.com/?size=100&id=uKPJ5HL65nlH&format=png&color=000000",size='large')
 
 
-col1, col2 = st.columns([3, 1])
-data = np.random.randn(10, 1)
+a, b = st.columns(2)
+c, d = st.columns(2)
 
-col1.subheader("A wide column with a chart")
-col1.line_chart(data)
+a.metric("Temperature", "30°F", "-9°F", border=True)
+b.metric("Wind", "4 mph", "2 mph", border=True)
 
-col2.subheader("A narrow column with the data")
-col2.write(data)
+c.metric("Humidity", "77%", "5%", border=True)
+d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
