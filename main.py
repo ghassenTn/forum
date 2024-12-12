4c2204d758ed378a4640bfee59051719112036a5
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2024-12-12 09:50:57 +0000

    details charts test

--- main.py
+++ main.py
@@ -63,6 +63,25 @@ with cnt_news:
     st.markdown("**Bar chart new release**")
     st.caption("By @DebianCommunity :material/forum:")
     st.pyplot(fake_plot())
+    with st.expander("Show code"):
+        st.code("""
+def fake_plot():
+    fig, ax = plt.subplots()
+    ax.bar(['A', 'B', 'C', 'D'], [10, 20, 15, 25], color=['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728'])
+    ax.set_title('Sample Bar Chart')
+    ax.set_xlabel('Categories')
+    ax.set_ylabel('Values')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_color('#dddddd')
+    ax.spines['bottom'].set_color('#dddddd')
+    ax.tick_params(axis='x', colors='#555555')
+    ax.tick_params(axis='y', colors='#555555')
+    ax.yaxis.grid(True, color='#eeeeee')
+    ax.xaxis.grid(False)
+    fig.tight_layout()
+    return fig
+""")
 
 cnt_bubels = st.container(border=True)
 with cnt_bubels:
