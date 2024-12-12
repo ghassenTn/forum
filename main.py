dd88f2c2e0a17d6de184d9fe0a1639c5fa6f591e
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2024-12-12 11:08:55 +0000

    update Aert welcome

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ with sb:
     sac.ButtonsItem(label='',icon='apple')
 ], label='', align='center')
     sac.rate(label='', value=3.5, align='center',half=True)
-    sac.alert(label='ghassenTn ', description=' Welcome  ', banner=True, icon=True, closable=True,size='xs',variant='transparent')
+    sac.alert(label='ghassenTn ', description=' Welcome  ', banner=True, icon=False, closable=True,size='xs',variant='transparent')
 
 
     
