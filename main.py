62b0df1223727c1136e60ba75bdaa36ac1b370aa
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2024-12-12 08:55:24 +0000

    update the chart

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import streamlit as st
 import matplotlib.pyplot as plt
 def fake_plot():
     fig, ax = plt.subplots()
-    ax.plot([1, 2, 3, 4])
+    ax.bar(['A', 'B', 'C', 'D'], [10, 20, 15, 25], color=['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728'])
+    ax.set_title('Sample Bar Chart')
+    ax.set_xlabel('Categories')
+    ax.set_ylabel('Values')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_color('#dddddd')
+    ax.spines['bottom'].set_color('#dddddd')
+    ax.tick_params(axis='x', colors='#555555')
+    ax.tick_params(axis='y', colors='#555555')
+    ax.yaxis.grid(True, color='#eeeeee')
+    ax.xaxis.grid(False)
+    fig.tight_layout()
     return fig
 
 st.markdown("## Debian Community .")
