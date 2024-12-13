ee0a6d3e1fd0d654c8c5cf596e175a5ab501cc9a
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2024-12-13 15:00:09 +0000

    add calender echart

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from random import randint
 import streamlit as st
 import matplotlib.pyplot as plt
 import streamlit_antd_components as sac
 from streamlit_echarts import st_echarts
-
+import pandas as pd
 
 sb = st.sidebar
 st.logo("https://avatars.githubusercontent.com/u/124514548?v=4",size='large')
@@ -141,7 +142,49 @@ st_echarts(options=options, height="500px")
 
 
 
+def get_virtual_data(year):
+    date_list = pd.date_range(
+        start=f"{year}-01-01", end=f"{year + 1}-01-01", freq="D"
+    )
+    return [[d.strftime("%Y-%m-%d"), randint(1, 10000)] for d in date_list]
 
+option = {
+    "tooltip": {"position": "top"},
+    "visualMap": {
+        "min": 0,
+        "max": 10000,
+        "calculable": True,
+        "orient": "horizontal",
+        "left": "center",
+        "top": "top",
+    },
+    "calendar": [
+        {"range": "2020", "cellSize": ["auto", 20]},
+        {"top": 260, "range": "2019", "cellSize": ["auto", 20]},
+        {"top": 450, "range": "2018", "cellSize": ["auto", 20], "right": 5},
+    ],
+    "series": [
+        {
+            "type": "heatmap",
+            "coordinateSystem": "calendar",
+            "calendarIndex": 0,
+            "data": get_virtual_data(2020),
+        },
+        {
+            "type": "heatmap",
+            "coordinateSystem": "calendar",
+            "calendarIndex": 1,
+            "data": get_virtual_data(2019),
+        },
+        {
+            "type": "heatmap",
+            "coordinateSystem": "calendar",
+            "calendarIndex": 2,
+            "data": get_virtual_data(2018),
+        },
+    ],
+}
+st_echarts(option, height="640px", key="echarts1")
 
     
             
